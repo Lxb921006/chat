aa1ffaa21a8fe42c2496df4052ab866993e29a6e
Lxb921006 <120332269@qq.com>
2023-08-11 23:46:33 +0800

    增加claude

--- src/views/chat/chat.vue
+++ src/views/chat/chat.vue
@@ -59,7 +59,7 @@
                         <transition name="el-zoom-in-top">
                             <div class="platform" @click="chatGptUrl()" v-if="data1.answer.length > 0">
                                 <p>
-                                    <svg class="icon-qa-3" aria-hidden="true"><use xlink:href="#icon-a-5_moxingtongbu"></use></svg> <span>ChatGPT-3.5</span>
+                                    <svg class="icon-qa-3" aria-hidden="true"><use xlink:href="#icon-a-5_moxingtongbu"></use></svg> <span>{{ data1.model }}</span>
                                 </p>
                             </div>
                         </transition>
@@ -156,6 +156,19 @@
                                 </el-switch>
                             </el-col>
                         </el-row>
+                        <el-row :gutter="10" class="set-item">
+                            <el-col :span="1" class="context-switch">模型选择: </el-col>
+                            <el-col :span="1">
+                                <el-select v-model="selectedModel" placeholder="请选择" class="c-select">
+                                    <el-option
+                                    v-for="item in modelAll"
+                                    :key="item.value"
+                                    :label="item.label"
+                                    :value="item.value">
+                                    </el-option>
+                                </el-select>
+                            </el-col>
+                        </el-row>
                         <el-button slot="reference">
                             <svg class="icon sessing-svg" aria-hidden="true">
                                 <use xlink:href="#icon-shezhi"></use>
@@ -224,6 +237,17 @@ export default {
             code:"",
             value: "text-davinci-003",
             cc: "<span class='cursor' v-show='this.cursor'>|</span>",
+            selectedModel: "claude",
+            modelAll: [
+                {
+                    value: 'claude-2',
+                    label: 'claude'
+                },
+                {
+                    value: 'chatGPT',
+                    label: 'chatGPT-3.5'
+                },
+            ],
             options: [{value: 'babbage', label: 'babbage'}, {value: 'davinci', label: 'davinci'}, {value: 'text-davinci-edit-001', label: 'text-davinci-edit-001'}, {value: 'babbage-code-search-code', label: 'babbage-code-search-code'}, {value: 'text-similarity-babbage-001', label: 'text-similarity-babbage-001'}, {value: 'code-davinci-edit-001', label: 'code-davinci-edit-001'}, {value: 'text-davinci-001', label: 'text-davinci-001'}, {value: 'ada', label: 'ada'}, {value: 'curie-instruct-beta', label: 'curie-instruct-beta'}, {value: 'babbage-code-search-text', label: 'babbage-code-search-text'}, {value: 'babbage-similarity', label: 'babbage-similarity'}, {value: 'whisper-1', label: 'whisper-1'}, {value: 'code-search-babbage-text-001', label: 'code-search-babbage-text-001'}, {value: 'text-curie-001', label: 'text-curie-001'}, {value: 'code-search-babbage-code-001', label: 'code-search-babbage-code-001'}, {value: 'text-ada-001', label: 'text-ada-001'}, {value: 'text-embedding-ada-002', label: 'text-embedding-ada-002'}, {value: 'text-similarity-ada-001', label: 'text-similarity-ada-001'}, {value: 'ada-code-search-code', label: 'ada-code-search-code'}, {value: 'ada-similarity', label: 'ada-similarity'}, {value: 'text-davinci-003', label: 'text-davinci-003'}, {value: 'code-search-ada-text-001', label: 'code-search-ada-text-001'}, {value: 'text-search-ada-query-001', label: 'text-search-ada-query-001'}, {value: 'davinci-search-document', label: 'davinci-search-document'}, {value: 'ada-code-search-text', label: 'ada-code-search-text'}, {value: 'text-search-ada-doc-001', label: 'text-search-ada-doc-001'}, {value: 'davinci-instruct-beta', label: 'davinci-instruct-beta'}, {value: 'text-similarity-curie-001', label: 'text-similarity-curie-001'}, {value: 'code-search-ada-code-001', label: 'code-search-ada-code-001'}, {value: 'ada-search-query', label: 'ada-search-query'}, {value: 'text-search-davinci-query-001', label: 'text-search-davinci-query-001'}, {value: 'curie-search-query', label: 'curie-search-query'}, {value: 'gpt-3.5-turbo-0301', label: 'gpt-3.5-turbo-0301'}, {value: 'davinci-search-query', label: 'davinci-search-query'}, {value: 'babbage-search-document', label: 'babbage-search-document'}, {value: 'ada-search-document', label: 'ada-search-document'}, {value: 'text-search-curie-query-001', label: 'text-search-curie-query-001'}, {value: 'text-search-babbage-doc-001', label: 'text-search-babbage-doc-001'}, {value: 'gpt-3.5-turbo', label: 'gpt-3.5-turbo'}, {value: 'curie-search-document', label: 'curie-search-document'}, {value: 'text-search-curie-doc-001', label: 'text-search-curie-doc-001'}, {value: 'babbage-search-query', label: 'babbage-search-query'}, {value: 'text-babbage-001', label: 'text-babbage-001'}, {value: 'text-search-davinci-doc-001', label: 'text-search-davinci-doc-001'}, {value: 'text-search-babbage-query-001', label: 'text-search-babbage-query-001'}, {value: 'curie-similarity', label: 'curie-similarity'}, {value: 'curie', label: 'curie'}, {value: 'text-similarity-davinci-001', label: 'text-similarity-davinci-001'}, {value: 'text-davinci-002', label: 'text-davinci-002'}, {value: 'davinci-similarity', label: 'davinci-similarity'}, {value: 'cushman:2020-05-03', label: 'cushman:2020-05-03'}, {value: 'ada:2020-05-03', label: 'ada:2020-05-03'}, {value: 'babbage:2020-05-03', label: 'babbage:2020-05-03'}, {value: 'curie:2020-05-03', label: 'curie:2020-05-03'}, {value: 'davinci:2020-05-03', label: 'davinci:2020-05-03'}, {value: 'if-davinci-v2', label: 'if-davinci-v2'}, {value: 'if-curie-v2', label: 'if-curie-v2'}, {value: 'if-davinci:3.0.0', label: 'if-davinci:3.0.0'}, {value: 'davinci-if:3.0.0', label: 'davinci-if:3.0.0'}, {value: 'davinci-instruct-beta:2.0.0', label: 'davinci-instruct-beta:2.0.0'}, {value: 'text-ada:001', label: 'text-ada:001'}, {value: 'text-davinci:001', label: 'text-davinci:001'}, {value: 'text-curie:001', label: 'text-curie:001'}, {value: 'text-babbage:001', label: 'text-babbage:001'}],
         }
     },
@@ -366,6 +390,7 @@ export default {
                 timeShow: false,
                 cid: "",
                 pid: "",
+                model: this.selectedModel,
             };
 
             this.clearS = setInterval(() => {
@@ -534,6 +559,51 @@ export default {
             this.getContentLen();
         },
         send () {
+            switch (this.selectedModel) {
+                case 'claude-2':
+                    this.sendClaude();
+                    break
+                case 'chatGPT':
+                    this.sendChatGpt();
+                    break
+            }
+            // let sendData = {};
+            // let lastData = [];
+            // let cacheData = JSON.parse(sessionStorage.getItem("chatCache"));
+            // if (this.contextSwitch) {
+            //     if (cacheData.length > 1) {
+            //     //发送的信息关联上下文
+            //         lastData = cacheData[cacheData.length - 2];
+            //         sendData = {cid: lastData.cid, pid: lastData.pid, data: this.chatContent, model: this.selectedModel};
+            //     } else {
+            //         // lastData = cacheData[cacheData.length];
+            //         sendData = {cid: "", pid: "", data: this.chatContent, model: this.selectedModel};
+            //     }
+            // } else {
+            //     sendData = {cid: "", pid: "", data: this.chatContent, model: this.selectedModel};
+            // }
+            
+            // this.socket.send(JSON.stringify(sendData));
+        },
+        sendClaude() {
+            let sendData = {};
+            let lastData = [];
+            // let cacheData = JSON.parse(sessionStorage.getItem("chatCache"));
+            if (this.contextSwitch) {
+                if (cacheData.length > 1) {
+                //发送的信息关联上下文
+                    // lastData = cacheData[cacheData.length - 2];
+                    sendData = {cid: "claude", pid: "", data: this.chatContent, model: this.selectedModel};
+                } else {
+                    // lastData = cacheData[cacheData.length];
+                    sendData = {cid: "claude", pid: "", data: this.chatContent, model: this.selectedModel};
+                }
+            } else {
+                sendData = {cid: "", pid: "", data: this.chatContent, model: this.selectedModel};
+            }
+            this.socket.send(JSON.stringify(sendData));
+        },
+        sendChatGpt() {
             let sendData = {};
             let lastData = [];
             let cacheData = JSON.parse(sessionStorage.getItem("chatCache"));
@@ -541,13 +611,13 @@ export default {
                 if (cacheData.length > 1) {
                 //发送的信息关联上下文
                     lastData = cacheData[cacheData.length - 2];
-                    sendData = {cid: lastData.cid, pid: lastData.pid, data: this.chatContent};
+                    sendData = {cid: lastData.cid, pid: lastData.pid, data: this.chatContent, model: this.selectedModel};
                 } else {
                     // lastData = cacheData[cacheData.length];
-                    sendData = {cid: "", pid: "", data: this.chatContent};
+                    sendData = {cid: "", pid: "", data: this.chatContent, model: this.selectedModel};
                 }
             } else {
-                sendData = {cid: "", pid: "", data: this.chatContent};
+                sendData = {cid: "", pid: "", data: this.chatContent, model: this.selectedModel};
             }
             
             this.socket.send(JSON.stringify(sendData));
