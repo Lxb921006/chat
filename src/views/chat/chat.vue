a7169ad5f99b003fa3ec586eb1ddfb387ec1cd0d
jay <120332269@qq.com>
2023-09-03 19:35:14 +0800

    更新

--- src/views/chat/chat.vue
+++ src/views/chat/chat.vue
@@ -1310,7 +1310,6 @@ export default {
             } else {
                 sendData = {cid: "", pid: "", data: this.chatContent.replace(/[\r\n\s]+/g, ''), model: this.selectedModel, content: ''};
             }
-            console.log(sendData);
             this.jumpFooter();
         },
         // 讯飞星火
